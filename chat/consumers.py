0b9fc0f24da3c6dd080ce1fd552a5879455706ce
Bishal Parajuli <parajulibishal00@gmail.com>
2026-05-10 12:30:58 +0545

    initial commit

---
+++ chat/consumers.py
@@ -0,0 +1,14 @@
+import json
+
+from asgiref.sync import sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        
+    async def disconnect(self, close_code):
+        pass
+    
+    async def receive(self, text_data=None, bytes_data=None):
+        pass
